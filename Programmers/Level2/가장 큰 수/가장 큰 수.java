a091cd4e8d3464f142280332c7c0efb1eadff60d
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-09-01 16:19:44 +0900

    Create 가장 큰 수.java

---
+++ Programmers/Level2/가장 큰 수/가장 큰 수.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        String[] result = new String[numbers.length];
+        
+        for (int i = 0; i < numbers.length; i++) {
+			result[i] = Integer.toString(numbers[i]);
+		}
+        
+        Arrays.sort(result, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if(result[0].equals("0")) return "0";
+        
+		for(String s : result) {
+            answer += s;
+        }
+				
+        return answer;
+        
+    }
+
+}
