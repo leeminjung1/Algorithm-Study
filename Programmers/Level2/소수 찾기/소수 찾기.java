d98e64329da1909b8dacd12086077edbca606a95
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-09-02 21:55:09 +0900

    Create 소수 찾기.java

---
+++ Programmers/Level2/소수 찾기/소수 찾기.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Solution {
+    public int solution(String numbers) {
+        int answer = 0;
+        HashSet<Integer> set = new HashSet<>();
+        permutation("", numbers, set);
+
+		while(set.iterator().hasNext()) {
+			int a = set.iterator().next();
+			set.remove(a);
+			if(a==2) answer++;
+			if(a%2!=0 && isPrime(a)) {
+				answer++;
+			}
+		}
+        
+        return answer;
+    }
+    
+    public void permutation(String prefix, String str, HashSet<Integer> set) {
+		int n = str.length();
+		if(!prefix.equals("")) {
+			set.add(Integer.valueOf(prefix));
+		}
+		for(int i=0; i<n; i++) {
+			permutation(prefix + str.charAt(i), str.substring(0,i)+str.substring(i+1,n), set);
+		}
+
+	}
+
+	public boolean isPrime(int num) {
+		if(num < 2) return false;
+		for(int i=2; i*i <= num; i++){
+			if(num % i == 0) return false;
+		}
+		return true;
+	}
+    
+
+}
