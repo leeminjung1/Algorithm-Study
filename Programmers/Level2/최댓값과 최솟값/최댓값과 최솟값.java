a1703d630c85211b3f1d30039b5ed08d9fc47e17
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-01 00:39:05 +0900

    Update 최댓값과 최솟값.java

--- Programmers/Level2/최댓값과 최솟값/최댓값과 최솟값.java
+++ Programmers/Level2/최댓값과 최솟값/최댓값과 최솟값.java
@@ -5,13 +5,13 @@ class Solution {
         String answer = "";
         String[] split = s.split("\\s");
 
-        int maxValue = Arrays.stream(Arrays.stream(split).mapToInt(Integer::parseInt).toArray())
-            .max()
-            .getAsInt();
+        int maxValue = Arrays.stream(split).mapToInt(Integer::parseInt)
+				.max()
+				.getAsInt();
 
-        int minValue = Arrays.stream(Arrays.stream(split).mapToInt(Integer::parseInt).toArray())
-            .min()
-            .getAsInt();
+		int minValue = Arrays.stream(split).mapToInt(Integer::parseInt)
+				.min()
+				.getAsInt();
 
 
         answer += minValue + " " + maxValue;
