bd9b68a48aec74df8d3e4056f17d7d8bee3aedeb
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-06 17:02:08 +0900

    Update 스킬트리.java

--- Programmers/Level2/스킬트리/스킬트리.java
+++ Programmers/Level2/스킬트리/스킬트리.java
@@ -1,21 +1,11 @@
+import java.util.*;
+
 class Solution {
     public int solution(String skill, String[] skill_trees) {
-        int answer = 0;
-        
-        for (String skillTree : skill_trees) {
-			String tempSkill = skillTree;
+        ArrayList<String> skillTrees = new ArrayList<>(Arrays.asList(skill_trees));
 
-			for (int i = 0; i < skillTree.length(); i++) {
-				String s = skillTree.substring(i, i+1);
-				if (!skill.contains(s)) {
-					tempSkill = tempSkill.replace(s, "");
-				}
-			}
+        skillTrees.removeIf(s -> skill.indexOf(s.replaceAll("[^" + skill + "]", "")) != 0);
 		
-			if (skill.indexOf(tempSkill) == 0)
-				answer++;
-		}
-        
-        return answer;
+		return skillTrees.size();
     }
 }
