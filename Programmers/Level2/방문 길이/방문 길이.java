fd62605d60db44319aeb2286137851a73cf13688
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-21 19:12:20 +0900

    Update 방문 길이.java

--- Programmers/Level2/방문 길이/방문 길이.java
+++ Programmers/Level2/방문 길이/방문 길이.java
@@ -1,129 +1,38 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 class Solution {
     public int solution(String dirs) {
-        int answer = dirs.length();
-		Dir UP = new Dir(0, 1, 'U');
-		Dir DOWN = new Dir(0, -1, 'D');
-		Dir LEFT = new Dir(-1, 0, 'L');
-		Dir RIGHT = new Dir(1, 0, 'R');
+        Map<Character, int[]> map = new HashMap<>();
+        map.put('U', new int[] {0, 1});
+		map.put('D', new int[] {0, -1});
+		map.put('R', new int[] {1, 0});
+		map.put('L', new int[] {-1, 0});
 
-		Dir now = new Dir(0, 0, null);
+        Set<String> set = new HashSet<>();        
+        int cx = 0;
+        int cy = 0;
 
-		HashSet<Dir> set = new HashSet<>();
+        for (char c : dirs.toCharArray()) {
+			int x = map.get(c)[0];
+			int y = map.get(c)[1];
 
-		for (char c : dirs.toCharArray()) {
+            cx += x;
+            cy += y;
 
-			int x=0;
-			int y=0;
-			if (c == 'U') {
-				x = now.getX() + UP.x;
-				y = now.getY() + UP.y;
-			}
-			if (c == 'D') {
-				x = now.getX()+DOWN.x;
-				y = now.getY()+DOWN.y;
-			}
-			if (c == 'L') {
-				x = now.getX()+LEFT.x;
-				y = now.getY()+LEFT.y;
-			}
-			if (c == 'R') {
-				x = now.getX()+RIGHT.x;
-				y = now.getY()+RIGHT.y;
-			}
-			if(x>5 || x<-5 || y>5 || y<-5) {
-				answer--;
-				continue;
-			}
-			now.setX(x);
-			now.setY(y);
-			now.setD(c);
-			if(set.contains(now)) {
-				answer--;
-				continue;
-			}
-			set.add(now);
-			Dir opp = new Dir(0,0,null);
-			if (c == 'U') {
-				opp = new Dir(now.getX(), now.getY() - 1, DOWN.getD());
-			}
-			if (c == 'D') {
-				opp = new Dir(now.getX(), now.getY() + 1, 'U');
-			}
-			if (c == 'L') {
-				opp = new Dir(now.getX()+1, now.getY(), 'R');
-			}
-			if (c == 'R') {
-				opp= new Dir(now.getX()-1, now.getY(), 'L');
-			}
-			set.add(opp);
-
-		}
-
-		return answer;
-	}
-
-}
-
-class Dir{
-	int x;
-	int y;
-	Character d;
-
-	public Dir(int x, int y, Character d) {
-		this.x = x;
-		this.y = y;
-		this.d = d;
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	public void setY(int y) {
-		this.y = y;
-	}
-
-	public Character getD() {
-		return d;
-	}
-
-	public void setD(Character d) {
-		this.d = d;
-	}
-
-	@Override
-	public String toString() {
-		return "Dir{" +
-				"x=" + x +
-				", y=" + y +
-				", d=" + d +
-				'}';
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if(this == obj) return true;
-		if (obj instanceof Dir) {
-			Dir that = (Dir) obj;
-			return this.getX() == that.getX() &&
-					this.getY() == that.getY() &&
-					this.getD() == that.getD();
-		}
-		return false;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(getX(), getY(), getD());
-	}
-
+            if(cx < -5 || cx > 5) {
+                cx -= x;
+                continue;
+            }
+            if(cy < -5 || cy > 5) {
+                cy -= y;
+                continue;
+            }
+            set.add((cx - x) + "" + (cy - y) + "" + cx + "" + cy);
+			set.add(cx + "" + cy + "" + (cx - x) + "" + (cy - y));
+        } 
+        return set.size()/2;
+    }
 }
