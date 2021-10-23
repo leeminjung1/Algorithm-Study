12d114d107c102dae8b6dc61b821d8e145fb46a3
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-23 19:47:36 +0900

    Update 행렬의 곱셈.java

--- Programmers/Level2/행렬의 곱셈/행렬의 곱셈.java
+++ Programmers/Level2/행렬의 곱셈/행렬의 곱셈.java
@@ -2,20 +2,14 @@ class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
 		
-        for (int i = 0; i < answer.length; i++) {
-          for (int j = 0; j < answer[0].length; j++) {
-            answer[i][j] = cij(arr1, arr2, i, j);
-          }
-        }
+		for (int i = 0; i < answer.length; i++) {
+			for (int j = 0; j < answer[0].length; j++) {
+				for (int k = 0; k < arr2.length; k++) {
+		        	answer[i][j] += arr1[i][k]*arr2[k][j];
+		        }
+			}
+		}
+        
         return answer;
-    }
-    
-    public int cij (int[][] arr1, int[][] arr2, int i, int j) {
-      int answer = 0;
-      for (int k = 0; k < arr2.length; k++) {
-        answer += arr1[i][k]*arr2[k][j];
-      }
-      return answer;
-    }
-    
+    }    
 }
