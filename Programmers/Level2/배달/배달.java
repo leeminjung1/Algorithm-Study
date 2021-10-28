676f2fc5587628da3c53122d85b231f2ca1b88bc
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-28 18:34:24 +0900

    Update 배달.java

--- Programmers/Level2/배달/배달.java
+++ Programmers/Level2/배달/배달.java
@@ -5,7 +5,7 @@ class Solution {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 if(i == j) graph[i][j] = 0;
-                graph[i][j] = 500001;
+                else graph[i][j] = 500001;
             }
         }
 
