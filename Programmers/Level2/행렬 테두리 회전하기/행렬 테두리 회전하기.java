c2571510f0ce7ec29876599198a87cb746f4a654
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-10-28 16:28:42 +0900

    Update 행렬 테두리 회전하기.java

--- Programmers/Level2/행렬 테두리 회전하기/행렬 테두리 회전하기.java
+++ Programmers/Level2/행렬 테두리 회전하기/행렬 테두리 회전하기.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 class Solution {
-    public int[] solution(int rows, int columns, int[][] queries) {
+    public static int[] solution(int rows, int columns, int[][] queries) {
         int[] answer = new int[queries.length];
         int[] map = new int[rows * columns];
         for (int i = 0; i < map.length; i++) {
@@ -9,68 +9,40 @@ class Solution {
 
         for (int i = 0; i < queries.length; i++) {
             map = rotation(queries[i], map, columns);
-            answer[i] = min(map, queries[i], columns);
+            answer[i] = min;
         }
+
         return answer;
     }
-    
-    public int[] rotation(int[] query, int[] map, int columns) {
+   
+    public static int min;
+
+    public static int[] rotation(int[] query, int[] map, int columns) {
         int[] newMap = map.clone();
-        int x1 = query[0];
-        int y1 = query[1];
-        int x2 = query[2];
-        int y2 = query[3];
+        min = map.length;
+        int upLeft = (query[0] - 1) * columns + query[1]-1;
+        int upRight = (query[0] - 1) * columns + query[3]-1;
+        int downRight = (query[2] - 1) * columns + query[3]-1;
+        int downLeft = (query[2] - 1) * columns + query[1]-1;
 
-        int up = newMap[(x1 - 1) * columns + y2-1];
-        int downRight = newMap[(x2 - 1) * columns + y2-1];
-        int downLeft = newMap[(x2 - 1) * columns + y1-1];
+        for (int i = upRight ; i > upLeft; i--) {
+            newMap[i] = map[i-1];
+            min = Math.min(newMap[i], min);
+        }
+        for (int i = downRight; i > upRight; i -= columns) {
+            newMap[i] = map[i - columns];
+            min = Math.min(newMap[i], min);
+        }
+        for (int i = downLeft; i < downRight; i++) {
+            newMap[i] = map[i+1];
+            min = Math.min(newMap[i], min);
+        }
+        for (int i = upLeft; i < downLeft; i += columns) {
+            newMap[i] = map[i + columns];
+            min = Math.min(newMap[i], min);
+        }
 
-        for (int i = (x1 - 1) * columns + y2-1 ; i > (x1 - 1) * columns + y1-1; i--) {
-            newMap[i] = newMap[i-1];
-        }
-      
-        for (int i = (x2 - 1) * columns + y2-1; i > (x1 - 1) * columns + y2-1; i -= columns) {
-            newMap[i] = newMap[i - columns];
-        }
-        newMap[(x1) * columns + y2-1] = up;
-
-        for (int i = (x2 - 1) * columns + y1-1; i < (x2 - 1) * columns + y2-1; i++) {
-            newMap[i] = newMap[i+1];
-        }
-        newMap[(x2 - 1) * columns + y2-2] = downRight;
-      
-        for (int i = (x1 - 1) * columns + y1 - 1; i < (x2 - 1) * columns + y1 - 1; i += columns) {
-            newMap[i] = newMap[i + columns];
-        }
-        newMap[(x2 - 2) * columns + y1 - 1] = downLeft;
-  
         return newMap;
-    }
-
-    public int min(int[] map, int[] query, int columns) {
-        int x1 = query[0];
-        int y1 = query[1];
-        int x2 = query[2];
-        int y2 = query[3];
-
-        int min = map.length;
-
-        for (int i = (x1 - 1) * columns + y1 - 1; i <= (x1 - 1) * columns + y2 - 1; i++) {
-            if(map[i] < min) min = map[i];
-        }
-
-        for (int i = (x1 - 1) * columns + y2-1; i <= (x2 - 1) * columns + y2-1; i+= columns) {
-            if(map[i] < min) min = map[i];
-        }
-
-        for (int i = (x2 - 1) * columns + y1-1; i <= (x2 - 1) * columns + y2-1; i++) {
-            if(map[i] < min) min = map[i];
-        }
-
-        for (int i = (x1 - 1) * columns + y1 - 1; i <= (x2 - 1) * columns + y1-1; i+=columns) {
-            if(map[i] < min) min = map[i];
-        }
-        return min;
     }
     
 }
