7647e395d93bee16f417d4136d0b91c3866684be
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-11-12 01:47:10 +0900

    Create Main.java

---
+++ Baekjoon/10989번: 수 정렬하기 3/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] arr = new int[10001];
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] > 0) {
+                sb.append(i).append('\n');
+                arr[i]--;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
