768020219ebf351dbb352b502661b8d1de7f0285
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-11-16 15:08:24 +0900

    Create Main.java

---
+++ Baekjoon/1149번: RGB거리/Main.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] cost = new int[n][3];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            cost[i][0] += Math.min(cost[i-1][1], cost[i-1][2]);
+            cost[i][1] += Math.min(cost[i-1][0], cost[i-1][2]);
+            cost[i][2] += Math.min(cost[i-1][1], cost[i-1][0]);
+        }
+
+        System.out.println(Math.min(cost[n-1][0], Math.min(cost[n-1][1], cost[n-1][2])));
+
+    }
+}
