eafa6b502d3e0bff61d9208757adffec5ddd6f4a
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-11-25 01:34:34 +0900

    Update Main.java

--- Baekjoon/11728번: 배열 합치기/Main.java
+++ Baekjoon/11728번: 배열 합치기/Main.java
@@ -1,34 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int[] a = new int[1000001];
+    static int[] b = new int[1000001];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[] a = new int[n];
-        int[] b = new int[m];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++) {
+        for (int i = n - 1; i >= 0; i--) {
             a[i] = Integer.parseInt(st.nextToken());
         }
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < m; i++) {
+        for (int i = m - 1; i >= 0; i--) {
             b[i] = Integer.parseInt(st.nextToken());
         }
-        int[] ans = new int[n + m];
-        System.arraycopy(a, 0, ans, 0, n);
-        System.arraycopy(b, 0, ans, n, m);
-        Arrays.sort(ans);
         StringBuilder sb = new StringBuilder();
-        for(int i : ans){
-            sb.append(i);
-            sb.append(" ");
+        int x = n - 1, y = m - 1;
+        while (x >= 0 && y >= 0) {
+            if (a[x] > b[y]) sb.append(b[y--]).append(" ");
+            else sb.append(a[x--]).append(" ");
         }
+        while (x >= 0) sb.append(a[x--]).append(" ");
+        while (y >= 0) sb.append(b[y--]).append(" ");
         System.out.println(sb);
     }
 }
