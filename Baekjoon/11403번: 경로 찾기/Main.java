d906125e7b1d74411bbed7994bff763b876a6a9e
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2021-12-15 02:47:02 +0900

    Create Main.java

---
+++ Baekjoon/11403번: 경로 찾기/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int n = Integer.parseInt(br.readLine());
+        int[][] node = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                node[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (node[i][k] == 1 && node[k][j] == 1) {
+                        node[i][j] = 1;
+                    }
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                sb.append(node[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+
+
+        System.out.print(sb);
+    }
+
+}
