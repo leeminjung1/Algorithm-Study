61c3f30181c3839b6d19691140c4bf4f17d6e4b8
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-02-23 15:49:13 +0900

    Create Main.java

---
+++ Baekjoon/15654번: N과 M (5)/Main.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        func(0, new boolean[n], new int[n]);
+
+        System.out.print(sb);
+
+    }
+
+    private static void func(int idx, boolean[] visited, int[] ans) {
+        if (idx == m) {
+            for (int i = 0; i < m; i++) {
+                sb.append(ans[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                ans[idx] = arr[i];
+                func(idx + 1, visited, ans);
+                visited[i] = false;
+            }
+        }
+    }
+
+}
