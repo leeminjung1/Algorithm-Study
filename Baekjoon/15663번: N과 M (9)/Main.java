a35a52be8209a31359b1b15f8ba7daf4821a0b23
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-03-01 10:44:08 +0900

    Create Main.java

---
+++ Baekjoon/15663번: N과 M (9)/Main.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int n, m;
+    static int[] arr, ans, visited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        ans = new int[m];
+        visited = new int[n];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        func(0);
+
+        System.out.println(sb);
+    }
+
+    private static void func(int depth) {
+        if (depth == m) {
+            print();
+            return;
+        }
+
+        for (int i = 0, t = -1; i < n; i++) {
+            if (visited[i] == 0 && arr[i] != t) {
+                visited[i] = 1;
+                ans[depth] = arr[i];
+
+                func(depth + 1);
+
+                visited[i] = 0;
+                t = arr[i];
+            }
+        }
+    }
+
+    private static void print() {
+        for (int i = 0; i < m; i++) {
+            sb.append(ans[i]).append(" ");
+        }
+        sb.append("\n");
+    }
+
+}
