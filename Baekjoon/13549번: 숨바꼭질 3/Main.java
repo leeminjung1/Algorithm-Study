dfe0b81700bbefc39aa4a84efff70ea4c0383543
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-03-16 21:28:14 +0900

    Update Main.java

--- Baekjoon/13549번: 숨바꼭질 3/Main.java
+++ Baekjoon/13549번: 숨바꼭질 3/Main.java
@@ -1,45 +1,28 @@
-import java.util.*;
- 
-public class Main {    
- 
-    static int min = Integer.MAX_VALUE;
-    static int n, k;
-    static boolean[] visited;
-    static int max = 100000;
-    
-    public static void main(String args[]) {
-        Scanner scan = new Scanner(System.in);
-        
-        n = scan.nextInt();
-        k = scan.nextInt();
-        
-        visited = new boolean[max + 1];
-        bfs();
-        System.out.println(min);
-    }
-    
-    public static void bfs() {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(n, 0));
-        
-        while(!q.isEmpty()) {
-            Node node = q.poll();
-            visited[node.x] = true;
-            if(node.x == k) min = Math.min(min, node.time);
-            
-            if(node.x * 2 <= max && visited[node.x * 2] == false) q.offer(new Node(node.x * 2, node.time));
-            if(node.x + 1 <= max && visited[node.x + 1] == false) q.offer(new Node(node.x + 1, node.time + 1));
-            if(node.x - 1 >= 0 && visited[node.x - 1] == false) q.offer(new Node(node.x - 1, node.time + 1));
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[K + 1];
+
+        for (int i = 0; i <= K; i++) {
+            if (i <= N) {
+                dp[i] = N - i;
+            } else {
+                dp[i] = dp[i - 1] + 1;
+                int t = (i & 1) == 0 ? dp[i >> 1] : dp[(i + 1) >> 1] + 1;
+                dp[i] = Math.min(dp[i], t);
+            }
         }
-    }
-    
-    public static class Node {
-        int x;
-        int time;
-        
-        public Node(int x, int time) {
-            this.x = x;
-            this.time = time;
-        }
+
+        System.out.println(dp[K]);
     }
 }
