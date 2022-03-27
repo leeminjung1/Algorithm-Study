5b24b186a235242ddf41a3c24865df04d8ac41ec
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-03-28 00:05:27 +0900

    Delete JadenCase 문자열 만들기.py

--- Programmers/Level2/JadenCase 문자열 만들기/JadenCase 문자열 만들기.py
+++
@@ -1,9 +0,0 @@
-def solution(s):
-    answer = ''
-    
-    for i in s.split(sep=" "):
-        answer += i.capitalize() + " ";
-        
-    answer = answer[:len(answer)-1]
-    
-    return answer
