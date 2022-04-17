d1ebd247ce70c77c745129c16e2fde059e022c68
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-04-18 01:54:55 +0900

    Update Main.java

--- Baekjoon/2638번: 치즈/Main.java
+++ Baekjoon/2638번: 치즈/Main.java
@@ -1,121 +1,64 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1};
-	static int[][] map;
-	static boolean[][] visited;
-	static ArrayList<Point> cheeseList;
-	static int cheeseCnt = 0;
+    private static int N, M;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static ArrayDeque<int[]> air = new ArrayDeque<>();
+    private static final int[] dx = { 1, -1, 0, 0 };
+    private static final int[] dy = { 0, 0, 1, -1 };
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = s.charAt(j * 2) - 48;
+            }
+        }
+        air.add(new int[] { 0, 0 });
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
+        int time = 0;
+        while (true) {
+            int size = air.size();
 
-		map = new int[N][M];
-		cheeseList = new ArrayList<>();
-		for(int i = 0; i < N; i++) {
-			for(int j = 0; j < M; j++) {
-				map[i][j] = sc.nextInt();
-				if(map[i][j] == 1) {
-					cheeseList.add(new Point(i, j));
-					cheeseCnt += 1;
-				}
-			}
-		}
+            while (size-- > 0) {
+                int[] tmp = air.poll();
+                dfs(tmp[0], tmp[1]);
+            }
 
-		int time = 0;
+            if (air.isEmpty()) {
+                break;
+            }
+            time++;
+        }
+        System.out.println(time);
+    }
 
-		while(cheeseCnt != 0) {
-			time++;
-			visited = new boolean[N][M];
-			dfs(0, 0); // 외부 공기 찾기 
-			// bfs();
-			melting(); // 치즈 녹이기 
-		}
+    private static void dfs(int x, int y) {
+        visited[y][x] = true;
 
-		System.out.println(time);
-	}
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
 
-	static void melting() {
-		for(int i = 0; i < cheeseList.size(); i++) {
-			int x = cheeseList.get(i).x;
-			int y = cheeseList.get(i).y;
-			int cnt = 0;
-
-			for(int j = 0; j < 4; j++) {
-				int nx = x + dx[j];
-				int ny = y + dy[j];
-
-				if(map[nx][ny] == 2) {
-					cnt++;
-				}
-			}
-
-			if(cnt >= 2) { // 외부 공기와 2변 이상 접촉한 경우 
-				map[x][y] = 0;
-				cheeseCnt--;
-				cheeseList.remove(i);
-				i--;
-			}
-		}
-	}
-
-
-	// dfs로 외부와 접촉한 공기 2로 표시
-	static void dfs(int x, int y) {
-		visited[x][y] = true;
-		map[x][y] = 2; // 외부 공기라는 의미로 2로 바꿔줌 
-
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
-			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-			if (visited[nx][ny] || map[nx][ny] == 1) continue; // 외내부 공기 판별을 위해 치즈인 경우도 pass 
-
-			dfs(nx, ny); // 공기인 경우만 dfs 수행 
-		}
-	}
-
-	// bfs로 외부와 접촉한 공기 2로 표시
-	public static void bfs() {
-		Queue<Point> queue = new LinkedList<>();
-		queue.add(new Point(0, 0));
-		visited[0][0] = true;
-		map[0][0] = 2;
-
-		while(!queue.isEmpty()) {
-			int x = queue.peek().x;
-			int y = queue.poll().y;
-
-			for(int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-				if (visited[nx][ny] || map[nx][ny] == 1) continue; // 외내부 공기 판별을 위해 치즈인 경우도 pass 
-
-				map[nx][ny] = 2; // 외부 공기라는 의미로 2로 바꿔줌 
-				queue.add(new Point(nx, ny)); // 공기인 경우만 큐에 넣어줌
-				visited[nx][ny] = true;
-			}
-		}
-	}
-
-}
-
-class Point {
-	int x;
-	int y;
-
-	Point(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
+            if ((nx >= 0 && nx < M && ny >= 0 && ny < N) && !visited[ny][nx]) {
+                if (map[ny][nx] != 0) {
+                    if (++map[ny][nx] == 3) {
+                        air.add(new int[]{nx, ny});
+                    }
+                } else {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
 }
