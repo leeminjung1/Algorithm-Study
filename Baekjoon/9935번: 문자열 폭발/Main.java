7fcf0db661c045a3d19d68a24f576b10c8f59bf7
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-04-20 02:07:48 +0900

    Update Main.java

--- Baekjoon/9935번: 문자열 폭발/Main.java
+++ Baekjoon/9935번: 문자열 폭발/Main.java
@@ -1,42 +1,36 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String line = br.readLine();
-		String regex = br.readLine();
-		int regexSize = regex.length();
-		
-		Stack<Character> st = new Stack<>();
-		
-		for(int i=0; i<line.length(); i++) {
-			st.push(line.charAt(i));
-			
-			// 폭발 문자열과 길이가 같아지면 탐색 시작 
-			if(st.size()>= regexSize) {
-				boolean flag = true;
-				
-				// st.size-regexSize부터 ~ st.size까지 탐색하여 regex와 일치하면 제거 
-				for(int j=0; j<regexSize; j++) {
-					if(st.get(st.size()-regexSize+j) != regex.charAt(j)) {
-						flag = false;
-						break;
-					}
-				}
-				if(flag) {
-					for(int j=0; j<regexSize; j++) {
-						st.pop();
-					}
-				}
-			}
-			
-		}
-		
-		StringBuilder sb = new StringBuilder();
-		for(Character c : st) {
-			sb.append(c);
-		}
-		System.out.println(sb.length()==0? "FRULA" : sb.toString());
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] str = br.readLine().toCharArray();
+        char[] arr = br.readLine().toCharArray();
+        char[] sol = new char[1000010];
+        int lastIdx = arr.length - 1;
+        int j = 0, k, q;
+        for (char c : str) {
+            sol[j] = c;
+            if (sol[j] == arr[lastIdx] && j >= lastIdx) {
+                k = lastIdx;
+                q = j;
+                while (k >= 0 && sol[q] == arr[k]) {
+                    q--;
+                    k--;
+                }
+                if (k < 0) j = q;
+            }
+            j++;
+        }
+        sol[j] = 0;
+
+        if (j == 0) {
+            System.out.println("FRULA");
+        } else {
+            char[] ans = Arrays.copyOfRange(sol, 0, j);
+            System.out.println(ans);
+        }
+    }
 }
