ba545bb82e9ada4a890f34caa4cbd8f3950d02a0
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-05-03 18:26:24 +0900

    Update Main.java

--- Baekjoon/2217번: 로프/Main.java
+++ Baekjoon/2217번: 로프/Main.java
@@ -1,26 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
- 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int cnt = Integer.parseInt(st.nextToken());
-        int arr[] = new int[cnt];
-        for(int i=0; i < cnt; i++) {
-            st = new StringTokenizer(br.readLine());
-            arr[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
+        int[] rope = new int[10001];
+        for (int i = 0; i < n; i++) {
+            rope[Integer.parseInt(br.readLine())]++;
         }
-        Arrays.sort(arr);
-        
-        long max = 0;
-        for(int i = cnt-1; i >= 0; i--) {
-            arr[i] = arr[i] * (cnt-i);
-            if(max < arr[i]) max = arr[i];
+
+        int max = 0;
+        for (int i = 10000, j = 0; i > 0; i--) {
+            j += rope[i];
+            max = Math.max(max, i * j);
         }
         System.out.println(max);
     }
