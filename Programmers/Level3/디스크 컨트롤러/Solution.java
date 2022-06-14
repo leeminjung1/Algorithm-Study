69c5ccdc5e78bcf9b5087c69e4a3296e020cfcc9
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2022-06-14 22:21:06 +0900

    Update Solution.java

--- Programmers/Level3/디스크 컨트롤러/Solution.java
+++ Programmers/Level3/디스크 컨트롤러/Solution.java
@@ -4,23 +4,22 @@ import java.util.PriorityQueue;
 
 class Solution {
     public int solution(int[][] jobs) {
-        int answer = 0, count = 0, end = 0, idx = 0;
+        int answer = 0, end = 0, idx = 0;
         Arrays.sort(jobs, Comparator.comparingInt(o -> o[0]));
         PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-        while (count < jobs.length) {
+        while (!(idx == jobs.length && pq.isEmpty())) {
             while (idx < jobs.length && jobs[idx][0] <= end) {
                 pq.add(jobs[idx++]);
             }
             if (pq.isEmpty()) {
                 end = jobs[idx][0];
-            } else {
-                int[] tmp = pq.poll();
-                answer += tmp[1] + end - tmp[0];
-                end += tmp[1];
-                count++;
+                continue;
             }
+            int[] tmp = pq.poll();
+            end += tmp[1];
+            answer += end - tmp[0];
         }
 
-        return aswer / jobs.length;
+        return answer / jobs.length;
     }
 }
