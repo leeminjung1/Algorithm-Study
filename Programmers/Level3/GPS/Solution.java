1aa4acd6a27bdedc79bd34c3f1278ae093d7d42d
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2023-01-11 15:35:59 +0900

    Update Solution.java

--- Programmers/Level3/GPS/Solution.java
+++ Programmers/Level3/GPS/Solution.java
@@ -1,33 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-
 class Solution {
     public int solution(int n, int m, int[][] edge_list, int k, int[] gps_log) {
 
-        ArrayList<ArrayList<Integer>> edge = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
-            edge.add(new ArrayList<>());
+        int[][] map = new int[n + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            map[edge_list[i][0]][edge_list[i][1]] = 1;
+            map[edge_list[i][1]][edge_list[i][0]] = 1;
         }
 
-        for (int[] e : edge_list) {
-            edge.get(e[0]).add(e[1]);
-            edge.get(e[1]).add(e[0]);
-        }
-
-        // dp[i][j] : i 초에 j 위치에 있을 수 있는 최소 오류 수정 횟수
         int[][] dp = new int[k][n + 1];
-        for (int[] d : dp) {
-            Arrays.fill(d, k + 1);
+        for(int i = 0; i < k; i++) {
+            for(int j = 0; j < n + 1; j++) {
+                dp[i][j] = k + 1;
+            }
         }
-
         dp[0][gps_log[0]] = 0;
-
+        
         for (int i = 1; i < k; i++) {
             for (int j = 1; j <= n; j++) {
                 dp[i][j] = Math.min(dp[i][j], dp[i - 1][j]);
-                for (Integer e : edge.get(j)) {
-                    dp[i][j] = Math.min(dp[i][j], dp[i - 1][e]);
+
+                for (int e = 1; e < n + 1; e++) {
+                    if (map[j][e] == 1) {
+                        dp[i][j] = Math.min(dp[i][j], dp[i - 1][e]);
+                    }
                 }
+
                 if (gps_log[i] != j) {
                     dp[i][j]++;
                 }
