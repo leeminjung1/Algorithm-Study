22e246e8ebbb10f547603b82e76a7b2681fc2952
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2023-04-06 16:42:34 +0900

    Create Main.java

---
+++ Baekjoon/20040번: 사이클 게임/Main.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int[] parent;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            if (!union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()))) {
+                System.out.println(i + 1);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+    private static boolean union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if (a == b) {
+            return false;
+        }
+
+        parent[a] = b;
+        return true;
+    }
+
+    private static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+
+        return parent[x] = find(parent[x]);
+    }
+}
