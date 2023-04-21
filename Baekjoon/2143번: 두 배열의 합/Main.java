db0f76f64e1bea96f518a4fc8ea8be1e44965fc0
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2023-04-21 21:21:39 +0900

    Create Main.java

---
+++ Baekjoon/2143번: 두 배열의 합/Main.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(st.nextToken());
+        }
+        int m = Integer.parseInt(br.readLine());
+        int[] b = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            b[i] = Integer.parseInt(st.nextToken());
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += a[j];
+                if (map.containsKey(sum)) {
+                    map.put(sum, map.get(sum) + 1);
+                } else {
+                    map.put(sum, 1);
+                }
+            }
+        }
+
+        long ans = 0;
+        for (int i = 0; i < m; i++) {
+            int num = t;
+            for (int j = i; j < m; j++) {
+                num -= b[j];
+                Integer cnt = map.get(num);
+                if (cnt != null) {
+                    ans += cnt;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
+}
