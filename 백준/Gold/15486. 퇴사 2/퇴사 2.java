a482adf7782be2c4950f30ae7b9439405e8f2256
leeminjung1 <52394879+leeminjung1@users.noreply.github.com>
2025-04-11 17:44:13 +0900

    [Gold V] Title: 퇴사 2, Time: 644 ms, Memory: 304288 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -8,17 +8,13 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		StringTokenizer st;
-		int[] dp = new int[n + 1];
+		int[] dp = new int[1500050];
 		for (int i = 0; i < n; i++) {
 			st = new StringTokenizer(br.readLine());
 			int t = Integer.parseInt(st.nextToken());
 			int p = Integer.parseInt(st.nextToken());
-
 			dp[i + 1] = Math.max(dp[i + 1], dp[i]);
-
-			if (i + t <= n) {
-				dp[i + t] = Math.max(dp[i + t], dp[i] + p);
-			}
+			dp[i + t] = Math.max(dp[i + t], dp[i] + p);
 
 		}
 		System.out.println(dp[n]);
